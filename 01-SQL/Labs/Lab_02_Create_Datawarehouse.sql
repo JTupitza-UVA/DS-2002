884d876bc8faea1530e69b2b639a52f1602be730
Jon Tupitza <98652016+JTupitza-UVA@users.noreply.github.com>
2022-09-05 17:02:39 -0400

    Add files via upload

---
+++ 01-SQL/Labs/Lab_02_Create_Datawarehouse.sql
@@ -0,0 +1,98 @@
+# DROP database `northwind_dw`;
+CREATE DATABASE `Northwind_DW` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE Northwind_DW;
+
+# DROP TABLE `dim_customers`;
+CREATE TABLE `dim_customers` (
+  `customer_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `business_phone` varchar(25) DEFAULT NULL,
+  `fax_number` varchar(25) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `first_name` (`first_name`),
+  KEY `last_name` (`last_name`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+
+
+# DROP TABLE `dim_employees`;
+CREATE TABLE `dim_employees` (
+  `employee_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `business_phone` varchar(25) DEFAULT NULL,
+  `home_phone` varchar(25) DEFAULT NULL,
+  `fax_number` varchar(25) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  `web_page` longtext,
+  PRIMARY KEY (`employee_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `first_name` (`first_name`),
+  KEY `last_name` (`last_name`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+
+# DROP TABLE `dim_products`;
+CREATE TABLE `dim_products` (
+  `product_key` int NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(25) DEFAULT NULL,
+  `product_name` varchar(50) DEFAULT NULL,
+  `standard_cost` decimal(19,4) DEFAULT '0.0000',
+  `list_price` decimal(19,4) NOT NULL DEFAULT '0.0000',
+  `reorder_level` int DEFAULT NULL,
+  `target_level` int DEFAULT NULL,
+  `quantity_per_unit` varchar(50) DEFAULT NULL,
+  `discontinued` tinyint(1) NOT NULL DEFAULT '0',
+  `minimum_reorder_quantity` int DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`product_key`),
+  KEY `product_code` (`product_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4;
+
+
+# DROP TABLE `dim_shippers`;
+CREATE TABLE `dim_shippers` (
+  `shipper_key` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`shipper_key`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+-- ----------------------------------------------------------------------
+-- TODO: JOIN the orders, order_details, order_details_status and 
+--       orders_status tables to create a new Fact Table in Northwind_DW.
+-- To keep things simple, don't include purchase order or inventory info
+-- ----------------------------------------------------------------------
+# DROP TABLE `fact_orders`;
+CREATE TABLE `fact_orders`;
