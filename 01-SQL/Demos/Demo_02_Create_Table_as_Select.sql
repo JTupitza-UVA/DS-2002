c62ffcd9b853f12277d0c1255a3a5b20caff3aa4
Jon Tupitza <98652016+JTupitza-UVA@users.noreply.github.com>
2023-02-07 15:15:42 +0000

    Add files via upload

---
+++ 01-SQL/Demos/Demo_02_Create_Table_as_Select.sql
@@ -0,0 +1,36 @@
+USE Northwind_DW3;
+
+CREATE TABLE fact_orders2 AS
+SELECT o.id,
+    o.employee_id,
+    o.customer_id,
+    od.product_id,
+    o.shipper_id,
+    o.ship_name,
+    o.ship_address,
+    o.ship_city,
+    o.ship_state_province,
+    o.ship_zip_postal_code,
+    o.ship_country_region,
+    od.quantity,
+    o.order_date,
+    o.shipped_date,
+    od.unit_price,
+    od.discount,
+    o.shipping_fee,
+    o.taxes,
+    o.payment_type,
+    o.paid_date,
+    o.tax_rate,
+    os.status_name AS order_status,
+    ods.status_name AS order_details_status
+FROM northwind.orders AS o
+INNER JOIN northwind.orders_status AS os
+ON o.status_id = os.id
+RIGHT OUTER JOIN northwind.order_details AS od
+ON o.id = od.order_id
+INNER JOIN northwind.order_details_status AS ods
+ON od.status_id = ods.id;
+
+
+SELECT * FROM northwind_dw3.fact_orders2;
