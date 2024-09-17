1f677289d5e4f600258a8407bb7061c85b924662
Jon Tupitza <wna8fw@virginia.edu>
2024-09-17 09:12:07 -0400

    Update Demo_02_Create_Table_as_Select.sql

--- 01-SQL/Demos/Demo_02_Create_Table_as_Select.sql
+++ 01-SQL/Demos/Demo_02_Create_Table_as_Select.sql
@@ -4,32 +4,27 @@ USE Northwind_DW_Demo;
 
 CREATE TABLE fact_orders AS
 SELECT o.id,
-    o.employee_id,
+	od.id,
     o.customer_id,
+    o.employee_id,
     od.product_id,
     o.shipper_id,
-    o.ship_name,
-    o.ship_address,
-    o.ship_city,
-    o.ship_state_province,
-    o.ship_zip_postal_code,
-    o.ship_country_region,
-    od.quantity,
     o.order_date,
+    o.paid_date,
     o.shipped_date,
+    o.payment_type,
+    od.quantity,
     od.unit_price,
     od.discount,
     o.shipping_fee,
     o.taxes,
-    o.payment_type,
-    o.paid_date,
     o.tax_rate,
     os.status_name AS order_status,
     ods.status_name AS order_details_status
 FROM northwind.orders AS o
 INNER JOIN northwind.orders_status AS os
 ON o.status_id = os.id
-RIGHT OUTER JOIN northwind.order_details AS od
+LEFT OUTER JOIN northwind.order_details AS od
 ON o.id = od.order_id
 INNER JOIN northwind.order_details_status AS ods
 ON od.status_id = ods.id;
