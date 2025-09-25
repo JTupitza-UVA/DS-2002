7311c2e2909bdfa819e2c294af5e790234e278a1
Jon Tupitza <98652016+JTupitza-UVA@users.noreply.github.com>
2025-09-25 09:34:21 -0400

    Update Lab_02e_Create_Merge_Stored_Procedures_ANSWER.sql

--- 01-SQL/Labs/Lab_02e_Create_Merge_Stored_Procedures_ANSWER.sql
+++ 01-SQL/Labs/Lab_02e_Create_Merge_Stored_Procedures_ANSWER.sql
@@ -11,7 +11,7 @@ DELIMITER $
 DROP PROCEDURE IF EXISTS merge_customers;
 
 CREATE PROCEDURE merge_customers(
-	IN customer_key INT,
+	IN cust_key INT,
     IN customer_id INT,
     IN company VARCHAR(50),
     IN last_name VARCHAR(50),
@@ -26,7 +26,7 @@ CREATE PROCEDURE merge_customers(
     IN country_region VARCHAR(50)
 )
 BEGIN
-    IF EXISTS (SELECT 1 FROM dim_customers WHERE customer_key = customer_key) THEN
+    IF EXISTS (SELECT 1 FROM dim_customers WHERE customer_key = cust_key) THEN
         UPDATE northwind_dw.dim_customers
         SET customer_id = customer_id,
 			company = company,
@@ -40,7 +40,7 @@ BEGIN
 			state_province = state_province,
 			zip_postal_code = zip_postal_code,
 			country_region = country_region
-        WHERE customer_key = customer_key;
+        WHERE customer_key = cust_key;
     ELSE
         INSERT INTO northwind_dw.dim_customers (
 			customer_id
@@ -73,6 +73,19 @@ END$$
 
 DELIMITER ;
 
+-- ---------------------------------------------------------------------
+-- Unit Test 'merge_customers'
+-- ---------------------------------------------------------------------
+-- Changes Area Code and 'Zip_Postal_Code' to accurate values.
+CALL merge_customers(29, 29, 'Company CC', 'Lee', 'Soo Jung', 'Purchasing Manager', '(123)555-0100', '(123)555-0101', '789 29th Street', 'Denver', 'CO', '99999', 'USA');
+-- Creates a New Customer Record
+CALL merge_customers(30, 30, 'Company DD', 'McCartney', 'Paul', 'Purchasing Manager', '(703)555-1234', '(703)555-1212', '258 30th Street', 'Alexandria', 'VA', '22314', 'USA');
+
+
+-- View the Results ----------------------    
+SELECT * FROM northwind_dw.dim_customers
+WHERE customer_key >= 29;
+
 -- -------------------------------------------------------------------
 -- Employees Dimension
 -- -------------------------------------------------------------------
@@ -81,7 +94,7 @@ DELIMITER $
 DROP PROCEDURE IF EXISTS merge_employees;
 
 CREATE PROCEDURE merge_employees(
-	IN employee_key INT,
+	IN emp_key INT,
     IN employee_id INT,
     IN company VARCHAR(50),
     IN last_name VARCHAR(50),
@@ -95,10 +108,11 @@ CREATE PROCEDURE merge_employees(
     IN city VARCHAR(50),
     IN state_province VARCHAR(50),
     IN zip_postal_code VARCHAR(15),
-    IN country_region VARCHAR(50)
+    IN country_region VARCHAR(50),
+    IN web_page LONGTEXT
 )
 BEGIN
-    IF EXISTS (SELECT 1 FROM dim_employees WHERE employee_key = employee_key) THEN
+    IF EXISTS (SELECT 1 FROM dim_employees WHERE employee_key = emp_key) THEN
         UPDATE northwind_dw.dim_employees
         SET employee_id = employee_id,
 			company = company,
@@ -113,11 +127,12 @@ BEGIN
 			city = city,
 			state_province = state_province,
 			zip_postal_code = zip_postal_code,
-			country_region = country_region
-        WHERE employee_key = employee_key;
+			country_region = country_region,
+            web_page = web_page
+        WHERE employee_key = emp_key;
     ELSE
         INSERT INTO northwind_dw.dim_employees (
-			customer_id
+			employee_id
             , company
             , last_name
             , first_name
@@ -130,9 +145,10 @@ BEGIN
             , city
             , state_province
             , zip_postal_code
-            , country_region)
+            , country_region
+            , web_page)
         VALUES (
-			customer_id
+			employee_id
             , company
             , last_name
             , first_name
@@ -145,11 +161,26 @@ BEGIN
             , city
             , state_province
             , zip_postal_code
-            , country_region);
+            , country_region
+            , web_page);
     END IF;
 END$$
 
 DELIMITER ;
+
+-- ---------------------------------------------------------------------
+-- Unit Test 'merge_employees'
+-- ---------------------------------------------------------------------
+-- Changes Area Code and 'Zip_Postal_Code' to accurate values.
+CALL merge_employees(9, 9, 'Northwind Traders', 'Hellung-Larsen', 'Anne', 'anne@northwindtraders.com', 'Sales Representative', '(206)555-0100', '(206)555-0102', '(206)555-0103', '123 9th Avenue', 'Seattle', 'WA', '98104', 'USA', 'http://northwindtraders.com#http://northwindtraders.com/#');
+-- Creates a New Customer Record
+CALL merge_employees(10, 10, 'Northwind Traders', 'McCartney', 'Paul', 'paul@northwindtraders.com', 'Purchasing Manager', '(206)555-1234', '(206)555-1212', '(206)555-4321', '258 30th Street', 'Seattle', 'WA', '98104', 'USA', 'http://northwindtraders.com#http://northwindtraders.com/#');
+
+
+-- View the Results ----------------------    
+SELECT * FROM northwind_dw.dim_employees
+WHERE employee_key >= 9;
+
 
 -- -------------------------------------------------------------------
 -- Products Dimension
@@ -159,7 +190,7 @@ DELIMITER $
 DROP PROCEDURE IF EXISTS merge_products;
 
 CREATE PROCEDURE merge_products(
-	IN product_key INT,
+	IN prod_key INT,
     IN product_id INT,
     IN product_code VARCHAR(25),
     IN product_name VARCHAR(50),
@@ -173,7 +204,7 @@ CREATE PROCEDURE merge_products(
     IN category VARCHAR(50)
 )
 BEGIN
-    IF EXISTS (SELECT 1 FROM dim_products WHERE product_key = product_key) THEN
+    IF EXISTS (SELECT 1 FROM dim_products WHERE product_key = prod_key) THEN
         UPDATE northwind_dw.dim_products
         SET product_id = product_id,
 			product_code = product_code,
@@ -186,7 +217,7 @@ BEGIN
 			discontinued = discontinued,
 			minimum_reorder_quantity = minimum_reorder_quantity,
 			category = category
-        WHERE product_key = product_key;
+        WHERE product_key = prod_key;
     ELSE
         INSERT INTO northwind_dw.dim_products (
 			product_id
@@ -217,6 +248,21 @@ END$$
 
 DELIMITER ;
 
+-- ---------------------------------------------------------------------
+-- Unit Test 'merge_products'
+-- ---------------------------------------------------------------------
+-- Changes Area Code and 'Zip_Postal_Code' to accurate values.
+CALL merge_products(45, 99, 'NWTSO-99', 'Northwind Traders Chicken Soup', '1.0000', '1.9500', '100', '200', '18.5 oz', '0', 50, 'Soups');
+-- Creates a New Customer Record
+CALL merge_products(46, 100, 'NWTSO-100', 'Northwind Traders Beef Soup', '1.0000', '1.9500', '100', '200', '18.5 oz', '0', 50, 'Soups');
+
+
+-- View the Results ----------------------    
+SELECT * FROM northwind_dw.dim_products
+WHERE product_key >= 45;
+
+
+
 -- -------------------------------------------------------------------
 -- Shippers Dimension
 -- -------------------------------------------------------------------
@@ -225,7 +271,7 @@ DELIMITER $
 DROP PROCEDURE IF EXISTS merge_shippers;
 
 CREATE PROCEDURE merge_shippers(
-	IN shipper_key INT,
+	IN ship_key INT,
     IN shipper_id INT,
     IN company VARCHAR(50),
     IN address LONGTEXT,
@@ -235,7 +281,7 @@ CREATE PROCEDURE merge_shippers(
     IN country_region VARCHAR(50)
 )
 BEGIN
-    IF EXISTS (SELECT 1 FROM dim_shippers WHERE shipper_key = shipper_key) THEN
+    IF EXISTS (SELECT 1 FROM dim_shippers WHERE shipper_key = ship_key) THEN
         UPDATE northwind_dw.dim_shippers
         SET shipper_id = shipper_id,
 			company = company,
@@ -244,7 +290,7 @@ BEGIN
             state_province = state_province,
 			zip_postal_code = zip_postal_code,
 			country_region = country_region
-        WHERE shipper_key = shipper_key;
+        WHERE shipper_key = ship_key;
     ELSE
         INSERT INTO northwind_dw.dim_shippers (
 			shipper_id
@@ -266,3 +312,16 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- ---------------------------------------------------------------------
+-- Unit Test 'merge_shippers'
+-- ---------------------------------------------------------------------
+-- Changes Area Code and 'Zip_Postal_Code' to accurate values.
+CALL merge_shippers(3, 3, 'Shipping Company C', '123 Any Street', 'Memphis', 'TN', '38103', 'USA');
+-- Creates a New Customer Record
+CALL merge_shippers(4, 4, 'Shipping Company C', '123 Any Street', 'Memphis', 'TN', '38103', 'USA');
+
+
+-- View the Results ----------------------    
+SELECT * FROM northwind_dw.dim_shippers
+WHERE shipper_key >= 3;
